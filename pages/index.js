b391dcb92b1ea89bb25fbd2b2206d176133cb568
wroobi <wojciech.wroblewski@hotmail.com>
2022-09-28 22:34:47 +0200

    hello

--- pages/index.js
+++ pages/index.js
@@ -12,9 +12,7 @@ export default function Home() {
 
       <main>
         <Header title="Welcome Tosia!" />
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
+        <p className="description">something</p>
       </main>
 
       <Footer />
