941effa91fae3f8515aad6e693affe8e90728975
wroobi <wojciech.wroblewski@hotmail.com>
2022-09-29 09:46:46 +0200

    update cypress test

--- cypress.config.js
+++ cypress.config.js
@@ -1,8 +1,12 @@
 const { defineConfig } = require("cypress");
 
+const ENV = process.env.NODE_ENV || "development";
+
+const PORT = ENV === "development" ? "3000" : "8888";
+
 module.exports = defineConfig({
   e2e: {
-    baseUrl: 'http://localhost:8888/',
+    baseUrl: `http://localhost:${PORT}/`,
     supportFile: false,
   },
 });
